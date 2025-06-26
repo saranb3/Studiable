21733f6a7faecd485ce1308a509d172804e07e35
saranb23 <148293225+saranb3@users.noreply.github.com>
2025-06-26 14:26:43 +0700

    Implemented the basic website with input and mock study spots

---
+++ next.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  allowedDevOrigins: ['http://192.168.1.61:3000'],
+};
+
+module.exports = nextConfig; 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,115 +1,95 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [currentInput, setCurrentInput] = useState(''); 
+  const [selectedLocation, setSelectedLocation] = useState(''); 
+
+  function handleEnterSearch(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      setSelectedLocation(currentInput);
+    }
+  }
+
+  function handleButtonClick() {
+    setSelectedLocation(currentInput);
+  }
+
+  function showLocation(){ 
+    if(selectedLocation) { 
+      return <p>Showing study spots near: {selectedLocation}</p>
+    }
+  }
+
+  const studySpots = [ 
+    {
+      name: "Starbucks", 
+      location: "101 เดอะเติร์ธเพลส ห้อง 112-A ชั้น 1", 
+      rating: 4.7,
+      Wifi: true, 
+      Coffee: true, 
+      Quiet: true, 
+      Outlets: true,
+      openTime: "8:00 AM - 10:00 PM"
+    }, 
+
+    {
+      name: "True Cafe", 
+      location: "ศูนย์การค้า วิสซ์ดอม วัน-โอ-วัน", 
+      rating: 4.6,
+      Wifi: true, 
+      Coffee: true, 
+      Quiet: true, 
+      Outlets: true,
+      openTime: "8:00 AM - 10:00 PM"
+    }, 
+
+    {
+      name: "Cataleya Estate", 
+      location: "88 Soi Punnawitthi 33 alley, Bang Chak, Phra Khanong, Bangkok 10260", 
+      rating: 4.8,
+      Wifi: true, 
+      Coffee: true, 
+      Quiet: true, 
+      Outlets: true,
+      openTime: "8:00 AM - 10:00 PM"
+    }, 
+  ];
+  
+  function showStudySpots(){ 
+    if(selectedLocation) { 
+      return (
+        <ul>
+          {studySpots.map((spot, idx) => (
+            <>
+              <li key={idx}>
+                <div>Name: {spot.name}</div>
+                <div>Location: {spot.location}</div>
+                <div>Rating: {spot.rating} ⭐ | Open time: {spot.openTime}</div>
+                <span>{spot.Wifi && <span className= "bg-yellow-300/30 text-amber-900 px-2 py-1 rounded">Wifi</span>} </span> 
+                <span>{spot.Coffee && <span className= "bg-yellow-300/30 text-amber-900 px-2 py-1 rounded">Coffee</span>} </span> 
+                <span>{spot.Outlets && <span className= "bg-yellow-300/30 text-amber-900 px-2 py-1 rounded">Outlets</span>} </span> 
+              </li>
+              <br />
+            </>
+          ))}
+        </ul>
+      );
+    }
+    return null;
+  }
+
+  
+  return ( 
+    <div>
+      <h1> Find a Studiable Space!</h1>
+      <input type="text" placeholder="Search by location"
+      value = {currentInput} 
+      onChange= {e => setCurrentInput(e.target.value)}
+      onKeyDown={handleEnterSearch}/>
+      <button onClick={handleButtonClick} >Search </button>
+      {showLocation()}
+      <br/>
+      {showStudySpots()}
     </div>
   );
 }
